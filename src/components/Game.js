8a72b19f481d85b32557913608bcb1ee5fe6c626
AndreaAguado <andrea95am@gmail.com>
2022-02-01 20:51:38 +0100

    add basic game html structure

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,6 +6,41 @@ const Game = () => {
             <div>
                 Juego
             </div>
+            <section>
+                <div>Grogu</div>
+                <div>[]</div>
+                <div>[]</div>
+                <div>[]</div>
+                <div>[]</div>
+                <div>[]</div>
+                <div>[]</div>
+                <div>
+                    Armario
+                    <div>Recipiente
+                        Caja de galletas x 3
+                        {/* <div>Caja de galletas</div>
+                        <div>Caja de galletas</div>
+                        <div>Caja de galletas</div> */}
+                    </div>
+                    <div>
+                        Recipiente
+                        Rana x 3
+                        {/* <div>Rana</div>
+                        <div>Rana</div>
+                        <div>Rana</div> */}
+                    </div>
+                    <div>
+                        Recipiente
+                        Huevo de rana x 3
+                        {/* <div>Huevo de rana</div>
+                        <div>Huevo de rana</div>
+                        <div>Huevo de rana</div> */}
+                    </div>
+                </div>
+            </section>
+            <section>
+                <button>Dado</button>
+            </section>
         </Main>
 
     )
