62012e66cae4a3a1ddbe42b0f2fb29bff3311234
AndreaAguado <andrea95am@gmail.com>
2022-02-01 14:45:38 +0100

    add header with Links to pages

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,33 @@ import Pieces from './Pieces';
 import Rules from './Rules';
 
 import { Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      hola mundo
+      <header>
+        Cuidado con Grogu!
+        <nav>
+          <ul>
+            <li>
+              <Link to="/rules">Reglas del juego</Link>
+            </li>
+            <li>
+              <Link to="/pieces">Fichas</Link>
+            </li>
+            <li>
+              <Link to="/game">Juego</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <Routes>
+        {/* <Route path="/" element={<div>Hola</div>}> */}
         <Route path="/rules" element={<Rules />} />
         <Route path="/game" element={<Game />} />
         <Route path="/pieces" element={<Pieces />} />
-
+        {/* </Route> */}
       </Routes>
     </div>
   );
