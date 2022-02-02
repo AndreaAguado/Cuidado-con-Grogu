8b191696fb0a8ee4738df8fd524af9e20d3c67c5
AndreaAguado <andrea95am@gmail.com>
2022-02-02 13:26:44 +0100

    remove commented code

--- src/components/game/Dice.js
+++ src/components/game/Dice.js
@@ -1,33 +1,7 @@
-// import { useState } from "react";
-
 import styles from '../../styles/game/Dice.module.scss';
 
 const Dice = (props) => {
-    // const [diceValue, setDiceValue] = useState({
-    //     numValue: "",
-    //     playValue: ""
-    // });
 
-    // const handleDice = () => {
-    //     let diceRoll = rollTheDice(4, 1);
-    //     console.log(diceRoll);
-    //     if (diceRoll === 1) {
-    //         setDiceValue({ numValue: 1, playValue: "Descargas una caja de galletas azules 🍪" });
-    //         props.handleDiceValue(diceRoll);
-    //     }
-    //     else if (diceRoll === 2) {
-    //         setDiceValue({ numValue: 2, playValue: "Descargas un huevo de rana (con mucho cuidado) 🥚" });
-    //         props.handleDiceValue(diceRoll);
-    //     }
-    //     else if (diceRoll === 3) {
-    //         setDiceValue({ numValue: 3, playValue: "Descargas una rana 🐸" });
-    //         props.handleDiceValue(diceRoll);
-    //     }
-    //     else {
-    //         setDiceValue({ numValue: 4, playValue: "Grogu se mueve una casilla 👣" });
-    //         props.handleDiceValue(diceRoll);
-    //     }
-    // }
     const handleDice = () => {
         let diceRoll = rollTheDice(4, 1);
         props.handleDiceValue(diceRoll);
