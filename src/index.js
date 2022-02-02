ed0a2a052e21b63966d5215ee2cb8e69d92c72d1
AndreaAguado <andrea95am@gmail.com>
2022-02-02 22:32:12 +0100

    change BrowserRouter by HashRouter for deployment in gh pages

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom';
 // import './index.scss';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-// import { HashRouter } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <HashRouter> */}
-    <BrowserRouter>
+    <HashRouter>
+      {/* <BrowserRouter> */}
       {/* <HashRouter> */}
       <App />
       {/* </HashRouter> */}
-    </BrowserRouter>
-    {/* </HashRouter> */}
+      {/* </BrowserRouter> */}
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
