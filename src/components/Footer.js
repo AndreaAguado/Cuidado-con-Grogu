cb0d883522481eb213fe6089796c0abdaae58619
AndreaAguado <andrea95am@gmail.com>
2022-02-03 21:56:24 +0100

    change year in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import styles from '../styles/layout/Footer.module.scss';
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <small className={styles.small}>Made with <span role="img" aria-label="emoji corazón" title="emoji corazón">&#10084;&#65039;</span> &copy; Andrea Aguado 2021</small>
+            <small className={styles.small}>Made with <span role="img" aria-label="emoji corazón" title="emoji corazón">&#10084;&#65039;</span> &copy; Andrea Aguado 2022</small>
             <nav>
                 <ul className={styles.links_list}>
                     <li><a className={styles.link} href="https://twitter.com/andrea114am" target="_blank" rel="noreferrer" title="twitter.com/andrea114am"><i className='bx bxl-twitter' ></i></a></li>
