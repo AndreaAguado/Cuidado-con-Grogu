2243047bdc6ff9466a7c72f232be8a6d768b4216
AndreaAguado <andrea95am@gmail.com>
2022-02-03 20:51:16 +0100

    add container

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -6,9 +6,8 @@ const Rules = () => {
     return (
         <Main>
             <h2>Reglas del juego</h2>
-            <section>
-
-                <p>
+            <section className={styles.container}>
+                <p className={styles.paragraph}>
                     El juego es muy sencillo. Dispondremos de 6 casillas que representan un camino en la zona de carga de la Razor Crest que lleva hasta el armario donde se encuentra segura la mercancía. De este modo tendremos 7 casillas, 6 del camino y una que representa al armario: 🔲🔲🔲🔲🔲🔲❎
                     Durante el viaje has hecho muy bien tu trabajo pero ahora debes vaciar el armario.
                     Para ello deberás descargar la mercancía poco a poco. Este es el momento más delicado ya que Grogu puede aprovechar y llegar hasta el armario. Si eso ocurre se comerá lo que encuentre y Mando perderá la oportunidad de saber dónde esta Ahsoka.
@@ -20,14 +19,14 @@ const Rules = () => {
                     <li>Recipiente de los huevos de rana: Tendrá dentro 3 huevos de rana (sin fertilizar)</li>
                     <li>Recipiente de las ranas: Tendrá dentro 3 ranas</li>
                 </ul>
-                <p>Además de estas piezas tendremos un dado de 4 caras donde cada cara representa una de las siguientes acciones:</p>
+                <p className={styles.paragraph}>Además de estas piezas tendremos un dado de 4 caras donde cada cara representa una de las siguientes acciones:</p>
                 <ul className={styles.list}>
                     <li>Descargas una caja de galletas azules 🍪</li>
                     <li>Descargas un huevo de rana (con mucho cuidado) 🥚</li>
                     <li>Descargas una rana 🐸</li>
                     <li>Grogu se mueve una casilla 👣</li>
                 </ul>
-                <p>
+                <p className={styles.paragraph}>
                     Además habrá una ficha que represente dónde está Grogu.
                     Ganarás el juego si consigues descargar toda la mercancía. Perderás si Grogu llega a la última casilla (el armario).
                 </p>
