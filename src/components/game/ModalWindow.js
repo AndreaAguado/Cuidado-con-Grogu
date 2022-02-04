1fa48a8214ab3e00ca966db705de982006f7dc2c
AndreaAguado <andrea95am@gmail.com>
2022-02-04 15:41:32 +0100

    enable close modal

--- src/components/game/ModalWindow.js
+++ src/components/game/ModalWindow.js
@@ -1,10 +1,20 @@
 import styles from '../../styles/game/ModalWindow.module.scss';
 
+import { useState } from 'react';
+
 function ModalWindow(props) {
+    const [hidden, setHidden] = useState(false);
+
+    const handleCloseModal = () => {
+        setHidden(true);
+    }
     return (
-        <div className={styles.modal}>
+        <div className={hidden ? styles.hidden : styles.modal}>
             <div className={styles.modal__dialog}>
                 <div className={styles.modal__content}>
+                    <header className={styles.header}>
+                        <div onClick={handleCloseModal} className={styles.close_button}><i className='bx bx-x'></i></div>
+                    </header>
                     <section>
                         {props.children}
                     </section>
